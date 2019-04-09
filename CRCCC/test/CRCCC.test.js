8127863c4795b52711825baa966e32b2bc56d597
avrezzon <anthonyrezz@gmail.com>
2019-04-09 01:03:44 -0600

    Basic tests are set up, except for the decimals test. To run the test do an npm install and then truffle test

--- CRCCC/test/CRCCC.test.js
+++ CRCCC/test/CRCCC.test.js
@@ -1,12 +1,34 @@
-//TODO tomorrow I will work more on this, setting up the mocha harness has been pretty weird
+const crccc_token = artifacts.require('CRCCC');
 
-var Token = artifacts.require("CRCCC");
+require('chai').should();
 
-contract("CryptoRamCashCoinChain Unit Tests", () =>{
+contract('CRCCC token', accounts => {
 
-    beforeEach(() => {
-        this.token = Token.deployed(_name, _symbol, _decimals).then((instance) => {
+    const _name = "CryptoRamCachCoin";
+    const _symbol = "CRCCC";
+    const _decimals = 2;
 
-        });
+    beforeEach(async () => {
+        this.crccc_token = await crccc_token.new(_name, _symbol, _decimals);
     });
+
+    describe('token attributes', () => {
+
+        it('has the correct name', async() =>{
+            const name = await this.crccc_token.name();
+            name.should.equal(_name);
+        });
+
+        it('has the correct symbol', async () =>{
+            const symbol = await this.crccc_token.symbol();
+            symbol.should.equal(_symbol);
+        });
+
+        it('has the correct decimals', async () =>{
+            const decimals = await this.crccc_token.decimals();
+            decimals.should.equal(_decimals);
+        });
+
+    });
+
 });

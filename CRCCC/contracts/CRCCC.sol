c528d93135dbd09018d5b5ecbfbc1f44413cbfad
avrezzon <anthonyrezz@gmail.com>
2019-04-10 18:27:57 -0600

    This modified the openzeppelin-solidity package to v2.0.1 that has support for the sol 0.4.25 compiler

--- CRCCC/contracts/CRCCC.sol
+++ CRCCC/contracts/CRCCC.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.4.25;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
@@ -6,7 +6,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 //This will be the CryptoRamCashCoinChain token contract
 contract CRCCC is ERC20, ERC20Detailed{
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20Detailed(_name, _symbol, _decimals) public{
+    constructor(string _name, string _symbol, uint8 _decimals) ERC20Detailed(_name, _symbol, _decimals) public{
 
     }
 

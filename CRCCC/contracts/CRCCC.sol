0640f3026f6a5f3f71e1be163d92da27e2821e7d
avrezzon <anthonyrezz@gmail.com>
2019-04-11 00:25:46 -0600

    Added a function to have the crccc contract spawn a new register with the vendor name as a param.  This is just an initial setup with the interactions between the contracts

--- CRCCC/contracts/CRCCC.sol
+++ CRCCC/contracts/CRCCC.sol
@@ -2,12 +2,21 @@ pragma solidity ^0.4.25;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "./Register.sol";
 
 //This will be the CryptoRamCashCoinChain token contract
 contract CRCCC is ERC20, ERC20Detailed{
+
+    //For now I think we said that there is one register per vendor
+    //If that assumption changes then it could be a mapping of address to an index space that contains an array of Registers
+    mapping (address => Register) vendorRegister;
 
     constructor(string _name, string _symbol, uint8 _decimals) ERC20Detailed(_name, _symbol, _decimals) public{
 
     }
 
+    //TODO maybe the manager needs to pay into the contract before they can spawn a new register
+    function spawnNewRegister(string _vendorName) public payable returns(bool){
+        vendorRegister[msg.sender] = new Register(_vendorName);
+    }
 }
--- CRCCC/contracts/Register.sol
+++ CRCCC/contracts/Register.sol
@@ -3,21 +3,25 @@ pragma solidity ^0.4.21;
 contract Register {
    
    address public manager;
-   
-   
-   
+   string public vendorName;
+
    struct Item{
        string Name;
        uint256 Number;
        uint256 Price;
    }
    
-    // invventory of items available for sale    
+    // inventory of items available for sale
     mapping (uint => Item) inventory;  
-    
+
+    //NOTE: do we need this, since we use this to index into the inventory and that is what is given as a param @ ln 34
     //can this be the item.Number and mapped by the item number? easier lookup and records like an array of ints
     uint count = 0;
-    
+
+    constructor(string _vendorName){
+        vendorName = _vendorName;
+    }
+
     //adds an item to the inventory
     //future improvment --> only manager
     function addItem(string name, uint256 num, uint256 price) public returns(bool){
@@ -28,10 +32,10 @@ contract Register {
   
   
     //gets item by order added to inventory need to know the count
-    function getItemByCount(uint count)public view returns(string name, uint256 num, uint256 price){
-        name = inventory[count].Name;
-        num = inventory[count].Number;
-        price = inventory[count].Price;
+    function getItemByCount(uint _count)public view returns(string name, uint256 num, uint256 price){
+        name = inventory[_count].Name;
+        num = inventory[_count].Number;
+        price = inventory[_count].Price;
     }
     
     

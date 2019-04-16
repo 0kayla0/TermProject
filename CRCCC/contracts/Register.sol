c8536546c3e612b0b3387e85600ec5601e632e3d
Kayla <kablack@rams.colostate.edu>
2019-04-15 21:22:20 -0600

    added constructor and changed item number

--- CRCCC/contracts/Register.sol
+++ CRCCC/contracts/Register.sol
@@ -1,53 +1,67 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.25;
 
 contract Register {
-   
-   address public manager;
-   
-   
-   
+
    struct Item{
        string Name;
        uint256 Number;
-       uint256 Price;
+       uint256 Price;  //what are reasonable price values for examples?
    }
-   
-    // invventory of items available for sale    
-    mapping (uint => Item) inventory;  
-    
-    //can this be the item.Number and mapped by the item number? easier lookup and records like an array of ints
-    uint count = 0;
-    
-    //adds an item to the inventory
-    //future improvment --> only manager
-    function addItem(string name, uint256 num, uint256 price) public returns(bool){
-        inventory[count] = Item(name, num, price);
-        count++;
-    } 
-  
-  
-  
-    //gets item by order added to inventory need to know the count
-    function getItemByCount(uint count)public view returns(string name, uint256 num, uint256 price){
-        name = inventory[count].Name;
-        num = inventory[count].Number;
-        price = inventory[count].Price;
+
+    // inventory of items available for sale
+    mapping (uint256 => Item) inventory;
+    uint256[] itemNumArray;
+
+    constructor() public{
+        //Basic inventory
+        uint number;
+        itemNumArray.push(number = createItemNumber("Cupcake"));
+        inventory[number] = Item("Cupcake ", number, 150);
+        itemNumArray.push(number = createItemNumber("Orange"));
+        inventory[number] = Item("Orange ", number, 50);
+        itemNumArray.push(number = createItemNumber("Swedish Meatball"));
+        inventory[number] = Item("Swedish Meatball ", number, 1000);
     }
-    
-    
-    //gets item by Name
-    // function getItemByName(string Name)public view returns(string name, uint256 num, uint256 price){
-        //if mapped by item.Number this changes
-   // }
-    
-    
-    //gets item by Number 
-    // function getItemByNumber(uint256 number)public view returns(string name, uint256 num, uint256 price){
-        //might change
-    //}
-    
-   
-    
-    
-  
+
+    //creates itemNumber from name of item
+    //converts to all lowercase
+    //https://ethereum.stackexchange.com/questions/10932/how-to-convert-string-to-int
+    //https://gist.github.com/ottodevs/c43d0a8b4b891ac2da675f825b1d1dbf
+    function createItemNumber(string name) internal pure returns (uint256 number){
+        bytes memory b = bytes(name);
+        for (uint i = 0; i < b.length; i++) {
+            //if uppercase add 32 so lowercase
+            if((b[i]>=65)&&(b[i]<=90)){
+                b[i] = bytes1(int(b[i])+32);
+            }
+            uint c = uint(b[i]);
+            number = c + number;
+        }
+        return number;
+    }
+
+
+    //adds an item to the inventory
+    //future improvement --> only manager
+    function addItem(string name, uint256 price) public{
+        uint256 number = createItemNumber(name);
+        inventory[number] = Item(name, number, price);
+        itemNumArray.push(number);
+    }
+
+
+     //gets item by Name
+      function getItemByName(string itemName)public view returns(string name, uint256 num, uint256 price){
+            uint256 number = createItemNumber(itemName);
+            name = inventory[number].Name;
+            num = inventory[number].Number;
+            price = inventory[number].Price;
+        }
+
+      function getItemByNumber(uint256 number)public view returns(string name, uint256 num, uint256 price){
+            name = inventory[number].Name;
+            num = inventory[number].Number;
+            price = inventory[number].Price;
+       }
+
 }

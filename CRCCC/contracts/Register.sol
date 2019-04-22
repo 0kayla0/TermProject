3d88956f6d27b2dd51168118a583f48ce979e7d8
Kayla <kablack@rams.colostate.edu>
2019-04-22 14:01:05 -0600

    fixed register.sol constructor

--- CRCCC/contracts/Register.sol
+++ CRCCC/contracts/Register.sol
@@ -8,27 +8,26 @@ contract Register {
    struct Item{
        string Name;
        uint256 Number;
-       uint256 Price;  //what are reasonable price values for examples?
+       uint256 Price;
    }
    
     // inventory of items available for sale
     mapping (uint => Item) inventory;  
     uint256[] itemNumArray;
-    
-    //NOTE: do we need this, since we use this to index into the inventory and that is what is given as a param @ ln 34
-    //can this be the item.Number and mapped by the item number? easier lookup and records like an array of ints
-    uint count = 0;
 
-    constructor(string _vendorName){
-        vendorName = _vendorName;
-    }
 
-    //adds an item to the inventory
-    //future improvment --> only manager
-    function addItem(string name, uint256 num, uint256 price) public returns(bool){
-        inventory[count] = Item(name, num, price);
-        count++;
-    } 
+      constructor(string _vendorName)public {
+            vendorName = _vendorName;
+            //Basic inventory
+            uint number;
+            itemNumArray.push(number = createItemNumber("Cupcake"));
+            inventory[number] = Item("Cupcake ", number, 150);
+            itemNumArray.push(number = createItemNumber("Orange"));
+            inventory[number] = Item("Orange ", number, 50);
+            itemNumArray.push(number = createItemNumber("Meatball"));
+            inventory[number] = Item("Meatball ", number, 1000);
+        }
+
     
     //gets item by order added to inventory need to know the count
     function getItemByCount(uint _count)public view returns(string name, uint256 num, uint256 price){
@@ -36,16 +35,6 @@ contract Register {
         num = inventory[_count].Number;
         price = inventory[_count].Price;
 
-    constructor() public{
-        //Basic inventory
-        uint number;
-        itemNumArray.push(number = createItemNumber("Cupcake"));
-        inventory[number] = Item("Cupcake ", number, 150);
-        itemNumArray.push(number = createItemNumber("Orange"));
-        inventory[number] = Item("Orange ", number, 50);
-        itemNumArray.push(number = createItemNumber("Swedish Meatball"));
-        inventory[number] = Item("Swedish Meatball ", number, 1000);
-    }
 
     //creates itemNumber from name of item
     //converts to all lowercase

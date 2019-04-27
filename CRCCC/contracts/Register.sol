72006e0913a1e9c97eea956fed26e443f2617df5
Kayla <kablack@rams.colostate.edu>
2019-04-27 17:59:51 -0600

    added ability to get inventory

--- CRCCC/contracts/Register.sol
+++ CRCCC/contracts/Register.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.25;
 
-contract Register {
-   
+
+contract Register{
+
    address public manager;
    string public vendorName;
 
@@ -9,13 +10,13 @@ contract Register {
        string Name;
        uint256 Number;
        uint256 Price;
-       uint256 count;
+       uint256 Count;
    }
-   
+
     // inventory of items available for sale
-    mapping (uint => Item) inventory;  
+    mapping (uint => Item) inventory;
     uint256[] itemNumArray;
-    uint256 totalSales;
+    uint256 _totalSales;
     uint256[] soldInventory;
 
       constructor(string _vendorName)public {
@@ -51,26 +52,27 @@ contract Register {
 
     //adds an item to the inventory
     //future improvement --> only manager
-    function addItem(string name, uint256 price) public{
+    function addItem(string name, uint256 price, uint256 count) public{
         uint256 number = createItemNumber(name);
-        inventory[number] = Item(name, number, price);
+        inventory[number] = Item(name, number, price, count);
         itemNumArray.push(number);
-
     }
 
-
      //gets item by Name
-      function getItemByName(string itemName)public view returns(string name, uint256 num, uint256 price){
+      function getItemByName(string itemName)public view returns(string name, uint256 num, uint256 price, uint256 count){
             uint256 number = createItemNumber(itemName);
             name = inventory[number].Name;
             num = inventory[number].Number;
             price = inventory[number].Price;
+            count = inventory[number].Count;
         }
 
-      function getItemByNumber(uint256 number)public view returns(string name, uint256 num, uint256 price){
+    //returns item by ItemNumber
+      function getItemByNumber(uint256 number)public view returns(string name, uint256 num, uint256 price, uint256 count){
             name = inventory[number].Name;
             num = inventory[number].Number;
             price = inventory[number].Price;
+            count = inventory[number].Count;
        }
 
       //given a name returns a price
@@ -80,11 +82,11 @@ contract Register {
       }
 
       //with purchase update count, add price to total sales, keep track of inventory sold
-      function purchase(string name)public view returns(bool success){
+      function purchase(string name)public returns(bool success){
         uint256 num = createItemNumber(name);
-        if(inventory[num].count > 0){
-            inventory[num].count = inventory[num].count -1;
-            totalSales = totalSales + inventory[num].Price;
+        if(inventory[num].Count > 0){
+            inventory[num].Count = inventory[num].Count -1;
+            _totalSales = _totalSales + inventory[num].Price;
             soldInventory.push(num);
             success = true;
         }else{
@@ -95,6 +97,78 @@ contract Register {
       //close register
 
 
-      //customer item lookup
+        //the "\n" is not working right format or return string is a comma separated string
+      //customer item lookup return name count price all items
+        function itemLookup() public view returns(string ){
+            //loop through items to add to string
+            string memory inventoryList;
+            for(uint i = 0; i < itemNumArray.length; i++){
+                string memory temp;
+                temp = append(temp, ", ", inventory[itemNumArray[i]].Name);
+                temp = append(temp, ", ", appendUintToString(inventory[itemNumArray[i]].Price));
+                temp = append(temp, ", ", appendUintToString(inventory[itemNumArray[i]].Count));
+                inventoryList = append(temp, '\n ', "\n");
+            }
+            return inventoryList;
+        }
+
+      //get inventory return name number price count all items
+      function getInventory() public view returns(string){
+        //loop through items add to string
+        string memory inventoryList;
+        for(uint i = 0; i < itemNumArray.length; i++){
+            string memory temp;
+            temp = append(temp, ", ", inventory[itemNumArray[i]].Name);
+            temp = append(temp, ", ", appendUintToString(inventory[itemNumArray[i]].Number));
+            temp = append(temp, ", ", appendUintToString(inventory[itemNumArray[i]].Price));
+            temp = append(temp, ", ", appendUintToString(inventory[itemNumArray[i]].Count));
+            inventoryList = append(temp, "\n", "\n");
+        }
+        return inventoryList;
+    }
+
+      //cashout return totalsales inventory sold [name, num, price, count] all items
+    function cashOut() public view returns(uint256 totalSales, string soldInventoryItems){
+            totalSales = _totalSales;
+            //loop through soldInventory
+            string memory inventoryList;
+        for(uint i = 0; i < soldInventory.length; i++){
+            string memory temp;
+            temp = append(temp, ", ", inventory[soldInventory[i]].Name);
+            temp = append(temp, ", ", appendUintToString(inventory[soldInventory[i]].Number));
+            temp = append(temp, ", ", appendUintToString(inventory[soldInventory[i]].Price));
+            temp = append(temp, ", ", appendUintToString(inventory[soldInventory[i]].Count));
+            inventoryList = append(temp, "\n", "\n");         //the "\n" is not working right
+        }
+            soldInventoryItems = inventoryList;
+    }
+
+    //helper
+    //https://ethereum.stackexchange.com/questions/10811/solidity-concatenate-uint-into-a-string
+    function appendUintToString(uint256 number) private pure returns (string str) {
+        uint maxlength = 78;
+        bytes memory reversed = new bytes(maxlength);
+        uint i = 0;
+        //number to bytes
+        while (number != 0) {
+            uint remainder = number % 10;
+            number = number / 10;
+            reversed[i++] = byte(48 + remainder);
+        }
+
+        bytes memory s = new bytes(i);
+        uint h;
+        //add name to byte string
+        for(h = 0; h < i; h++) {
+            s[h] = reversed[i-1-h];
+        }
+        str = string(s);
+    }
+
+
+      //https://ethereum.stackexchange.com/questions/729/how-to-concatenate-strings-in-solidity
+    function append(string a, string b, string c)private pure returns (string){
+        return string(abi.encodePacked(a, b, c));
+    }
 
 }
--- CRCCC/test/TestRegister.sol
+++ CRCCC/test/TestRegister.sol
@@ -12,24 +12,21 @@ contract TestRegister {
            string Name;
            uint256 Number;
            uint256 Price;
+           uint256 Count;
        }
 
     function testInitialSetup()public{
-
         Register reg = new Register("lola");
         Assert.equal(reg.vendorName(), "lola", "Name is set");
-
-
     }
 
     //I can't figure out how to get the name, num, price back from
     //getitembynumber and getitembyname in the register contract
     //because I don't think solidity supports passing strings between
     //contracts they have to be byte32
-
     function testAddItem() public{
         Register reg = new Register("lola");
-        reg.addItem("banana", 25);
+        reg.addItem("banana", 25, 10);
     }
 
 }

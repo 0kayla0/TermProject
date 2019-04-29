3ff66f99d9f1a6f8040b61aecabdad212ff40437
avrezzon <anthonyrezz@gmail.com>
2019-04-28 22:36:10 -0600

    Added in the network settings for the rinkeby testnet.  Started the refactor the mocha test of the register contract.

---
+++ CRCCC/test/Register.test.js
@@ -0,0 +1,15 @@
+const register = artifacts.require('./CRCCC.sol');
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+let accounts;
+
+require('chai').should();
+
+contract("", accounts => {
+
+
+});

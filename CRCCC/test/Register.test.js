6a1bfa352ec2a32239011b2162ce013ce6e44a1a
avrezzon <anthonyrezz@gmail.com>
2019-04-29 18:59:51 -0600

    Removed not used imports

--- CRCCC/test/Register.test.js
+++ CRCCC/test/Register.test.js
@@ -1,15 +1,10 @@
 const Register = artifacts.require('./Register.sol');
-const assert = require("assert");
 const ganache = require("ganache-cli");
-const Web3 = require("web3");
-const provider = ganache.provider();
-const web3 = new Web3(provider);
 
-let accounts;
 
 require('chai').should();
 
-contract("Register contract", accounts => {
+contract("Register contract", () => {
 
     let register;
 

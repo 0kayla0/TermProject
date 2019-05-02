9fdf06c207af97b91a1bfc6f3094d238a5a95e08
avrezzon <anthonyrezz@gmail.com>
2019-05-01 18:59:01 -0600

    Setting up the components for the react app.  solidifying the structure

--- crccc-token/src/App.js
+++ crccc-token/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import CurrentVendor from "./Components/CurrentVendor";
+import CloseRegister from "./Components/CloseRegister";
+import SpawnRegister from "./Components/SpawnRegister";
+import PurchaseItem from "./Components/PurchaseItem";
+import RegisterStudent from "./Components/RegisterStudent";
+import ItemsList from "./Components/ItemsList";
+import StudentBalance from "./Components/StudentBalance";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        acct_address : "", //will need to query
+        acct_name : "",
+        cur_register : "",
+        available_reg : [] //will need to query the contract to find avaiable
+    };
+  }
+
+  render(){
+    return (
+        <div>
+          <p>Lets get this bread</p>
+          <CurrentVendor/>
+          <SpawnRegister/>
+          <PurchaseItem/>
+          <RegisterStudent/>
+          <ItemsList/>
+          <StudentBalance/>
+          <CloseRegister/>
+        </div>
+    );
+  }
+
 }
 
-export default App;

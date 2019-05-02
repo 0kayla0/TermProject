ebbb3dbf66b1eeb00f6f2002479a24ab7c0f83a3
avrezzon <anthonyrezz@gmail.com>
2019-05-01 19:35:12 -0600

    designed what information state needs to hold. will need to follow up with adding elements for the actual contracts.

--- crccc-token/src/App.js
+++ crccc-token/src/App.js
@@ -11,24 +11,49 @@ export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-        acct_address : "", //will need to query
         acct_name : "",
-        cur_register : "",
+        register_name : "",
         available_reg : [] //will need to query the contract to find avaiable
     };
-  }
+
+    //TODO need to have things that keep info about the contracts => implement with drizzle
+
+    this.updateRegister = this.updateRegister().bind(this);
+    this.updateAvailableRegisters = this.updateAvailableRegisters().bind(this);
+    this.updateAccountName = this.updateAccountName().bind(this);
+
+  };
+
+  updateRegister(reg){
+    this.setState({register_name: reg});
+  };
+
+  updateAvailableRegisters(){
+    //TODO
+  };
+
+  updateAccountName(name){
+    this.setState({acct_name: name});
+  };
 
   render(){
     return (
         <div>
+
           <p>Lets get this bread</p>
-          <CurrentVendor/>
-          <SpawnRegister/>
-          <PurchaseItem/>
-          <RegisterStudent/>
-          <ItemsList/>
+
+          <CurrentVendor register_name={this.state.register_name} available_registers={this.state.available_reg} updateRegister={this.updateRegister()}/>
+
+          <RegisterStudent acc_name={this.state.acct_name} update_name={this.updateAccountName()}/>
+          <SpawnRegister updateAvailableRegisters={this.updateAvailableRegisters()}/>
+
+          <ItemsList register_name={this.state.register_name}/>
+          <PurchaseItem register_name={this.state.register_name}/>
+
           <StudentBalance/>
-          <CloseRegister/>
+
+          <CloseRegister register_name={this.state.register_name}/>
+
         </div>
     );
   }
--- crccc-token/src/Components/CurrentVendor.js
+++ crccc-token/src/Components/CurrentVendor.js
@@ -1,5 +1,8 @@
 import React,{Component} from 'react';
 
+//This component will be a dropdown of the available registers
+//The register that the user selects will be the one that the user will be doing transactions with
+
 export default class CurrentVendor extends Component{
     constructor(props){
         super(props);

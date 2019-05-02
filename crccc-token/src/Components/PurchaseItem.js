cd08cd70a6dd05778706c16b9bec6ac0749a09b7
avrezzon <anthonyrezz@gmail.com>
2019-05-02 02:27:55 -0600

    Able to see the cart and what items are in it along with price

--- crccc-token/src/Components/PurchaseItem.js
+++ crccc-token/src/Components/PurchaseItem.js
@@ -15,10 +15,24 @@ export default class PurchaseItem extends Component{
         let temp_curItem = this.state.currentItem;
         temp_cart.push(temp_curItem);
         temp_curItem = "Enter Item";
-        this.setState({cart : [...temp_cart]});
+
+        console.log("Adding " + temp_curItem + " to the cart");
+        console.log(temp_cart);
+
+        this.setState({cart : temp_cart});
         this.setState({currentItem : temp_curItem});
+        console.log("yee")
     }
 
+    itemPrice(itemName){
+        let price = 0;
+        let tempInventory = [...this.props.inventory];
+        let results = tempInventory.filter(item => (itemName === item.name));
+        if(results.length !== 0){
+            price = results[0].price;
+        }
+        return price;
+    }
 
     updateCurItem(event){
         this.setState({currentItem : event.target.value});
@@ -28,17 +42,35 @@ export default class PurchaseItem extends Component{
     render(){
 
         let total_price = 0;
+        let transaction;
 
-        let transaction = this.state.cart.map((item) => (
-            <tr>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-            </tr>
-        ));
-
-        for(let i = 0; i < this.props.inventory.length; i++){
-            console.log("whoa")
+        //FIXME <td>{item.price}</td>
+        if(this.state.cart.length !== 0){
+            transaction = this.state.cart.map((item) => (
+                <tr>
+                    <td>{item}</td>
+                    <td>{this.itemPrice(item)}</td>
+                </tr>
+            ));
+        }else{
+            transaction =(
+                <tr>
+                    <td>There is currently nothing in your cart</td>
+                    <td>0</td>
+                </tr>
+            );
         }
+
+        // for(let i = 0; i < this.state.cart.length; i++){
+        //     total_price += this.itemPrice(this.state.cart[i]);
+        // }
+        //
+        // let total_row= (
+        //     <tr>
+        //         <td>ORDER TOTAL</td>
+        //         <td>{total_price}</td>
+        //     </tr>
+        // );
 
         return (
             <div>
@@ -55,7 +87,7 @@ export default class PurchaseItem extends Component{
                         </tr>
                     </thead>
                     <tbody>
-
+                        {transaction}
                     </tbody>
                 </Table>
             </div>

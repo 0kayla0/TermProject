a24f79203edfae40761abaf170a9b02d3a870663
avrezzon <anthonyrezz@gmail.com>
2019-05-01 21:31:31 -0600

    layout

--- crccc-token/src/App.js
+++ crccc-token/src/App.js
@@ -53,34 +53,24 @@ export default class App extends Component {
 
                 <Container>
                     <Row>
-                        <Col>.col</Col>
+                        <Col>
+                            <RegisterStudent acc_name={this.state.acct_name} update_name={this.updateAccountName}/>
+                        </Col>
+                        <Col>
+                            <SpawnRegister updateAvailableRegisters={this.updateAvailableRegisters}/>
+                        </Col>
                     </Row>
+
                     <Row>
-                        <Col>.col</Col>
-                        <Col>.col</Col>
-                        <Col>.col</Col>
-                        <Col>.col</Col>
+                        <Col>
+                            <CurrentVendor register_name={this.state.register_name} available_registers={this.state.available_reg}
+                                           updateRegister={this.updateRegister}/>
+                        </Col>
+                        <Col>
+                            <StudentBalance/>
+                        </Col>
                     </Row>
-                    <Row>
-                        <Col xs="3">.col-3</Col>
-                        <Col xs="auto">.col-auto - variable width content</Col>
-                        <Col xs="3">.col-3</Col>
-                    </Row>
-                    <Row>
-                        <Col xs="6">.col-6</Col>
-                        <Col xs="6">.col-6</Col>
-                    </Row>
-                    <Row>
-                        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-                        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-                        <Col sm="4">.col-sm-4</Col>
-                    </Row>
-                    <Row>
-                        <Col sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .order-sm-2 .offset-sm-1</Col>
-                    </Row>
-                    <Row>
-                        <Col sm="12" md={{ size: 6, offset: 3 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
-                    </Row>
+
                     <Row>
                         <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
                         <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
@@ -97,16 +87,10 @@ export default class App extends Component {
                     </CardBody>
                 </Card>
 
-                <CurrentVendor register_name={this.state.register_name} available_registers={this.state.available_reg}
-                               updateRegister={this.updateRegister}/>
 
-                <RegisterStudent acc_name={this.state.acct_name} update_name={this.updateAccountName}/>
-                <SpawnRegister updateAvailableRegisters={this.updateAvailableRegisters}/>
 
                 <ItemsList register_name={this.state.register_name}/>
                 <PurchaseItem register_name={this.state.register_name}/>
-
-                <StudentBalance/>
 
                 <CloseRegister register_name={this.state.register_name}/>
 
